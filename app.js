1a35ca7505060e8abb5983e16c4139eac27a4e4d
boklos <boklos@github.com>
2015-08-11 18:23:14 -0400

    4

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient; // created a var object to handle through it the methods
 
 // Open the connection to the server
-MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
-    if(err) throw err;
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) { // function(err,db) is a callback fn, executes after the connection
+    if(err) throw err;                                                   // now the data is in var object "db" where we handle the database
 
     // Find one document in our collection
     db.collection('coll').findOne({}, function(err, doc) {
