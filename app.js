681b1984759edc75d2f84378a2ba9895b82ebe26
boklos <boklos@github.com>
2015-08-20 11:57:52 -0400

    notes

--- app.js
+++ app.js
@@ -10,13 +10,15 @@ db.collection('data').find().sort([["State" , 1] , ["Temperature" , -1]]).toArra
     var res={};
     var newstate="";
     var month_high = [];
+    //console.log(JSON.stringify(docs)); // an array of objects [{id="",State="", temperature="" etc..},{id="",state="",temperature="",etc..},etc]
+
 
     for(var i=0; i < docs.length; i++) {
         //console.log("Doc : " + docs);
-        var temperature = docs[i]["Temperature"];
+        //var temperature = docs[i]["Temperature"];
 
         if(newstate != docs[i]["State"]) {
-            month_high.push(docs[i]);
+            month_high.push(docs[i]); //update the month_high array length with the whole correspondent document object [{...},{...},{...}]
         }
             // update row
             newstate = docs[i]["State"];
@@ -24,15 +26,17 @@ db.collection('data').find().sort([["State" , 1] , ["Temperature" , -1]]).toArra
 
      var numCallbacks = 0;
      for (i=0; i<month_high.length; i++) {
-        db.collection('data').update(month_high[i], {"$set" : {"month_high" : true}},
+        db.collection('data').update(month_high[i], {"$set" : {"month_high" : true}},//updates the corresponding document in month_hgh array
             function(err, updated){
                 if(err) throw err;
-                console.log("Updated" + updated + "document month high");
+                console.log("Updated " + updated + " document month high");
                 if (++numCallbacks == month_high.length){
                     return db.close();
                 }
 
             });
+
+        console.log(JSON.stringify(month_high[i]["State"]));
      }
 
  });
